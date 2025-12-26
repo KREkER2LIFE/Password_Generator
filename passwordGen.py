1c3abe95ff8a406d883fa05a5eeab665379f6671
Dodogo <denislesan2022@gmail.com>
2025-12-26 05:44:31 +0200

    Did some stuff such adding icon + a text label

--- passwordGen.py
+++ passwordGen.py
@@ -1,4 +1,4 @@
-import tkinter as tk 
+from tkinter import  * 
 import string 
 import random 
 
@@ -8,9 +8,12 @@ all_characters = list(ascii_characters)
 
 print("This is the password generator program.")
 
-window = tk.Tk()
+window = Tk()
 window.title("Password Generator/v.1")
 window.geometry("500x250")
+window.iconbitmap(default="temporarily_icon.png")
+label = Label(window , text="How many characters you wanna use: ")
+label.pack()
 
 
 
