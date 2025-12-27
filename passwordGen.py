d1de4d559a49602ea5c05e90d84a5729f69d6848
Dodogo <denislesan2022@gmail.com>
2025-12-27 16:25:48 +0200

    add some style

--- passwordGen.py
+++ passwordGen.py
@@ -11,11 +11,19 @@ print("This is the password generator program.")
 window = Tk()
 window.title("Password Generator/v.1")
 window.geometry("500x250")
-window.iconbitmap(default="temporarily_icon.png")
-label = Label(window , text="How many characters you wanna use: ")
+
+icon = PhotoImage(file = "temporarily_icon.png")
+window.iconphoto(False, icon)
+window.attributes("-alpha", 0.8)
+window.configure(background="black")
+
+label = Label(window , text="How many characters you wanna use: " , fg="white" ,background="black")
 label.pack()
 
+entry = Entry()
+entry.pack()
 
+n = entry.get()
 
 while True:    
     
